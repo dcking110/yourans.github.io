c6eaa78bab0ab0d31d2d59748287197b25724cd5
dcking110 <dcking110@163.com>
2023-12-11 22:38:45 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+// 初始化数字显示为？
+document.getElementById('randomNumber').textContent = '?';
+
 document.getElementById('generateButton').addEventListener('click', function() {
     // 生成1到250的随机数字
     var randomNumber = Math.floor(Math.random() * 250) + 1;
 
+    // 提取纯数字
+    var numberText = randomNumber.toString();
+
     // 显示随机数字
-    document.getElementById('randomNumber').textContent = 'Random Number: ' + randomNumber;
+    document.getElementById('randomNumber').textContent = numberText;
 });
